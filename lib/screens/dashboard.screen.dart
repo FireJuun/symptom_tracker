d5d0300666cf1ed18de75536eed5ac6974f6338b
John Manning <john.d.manning@gmail.com>
2020-05-07 02:22:38 -0400

    dashboard listviews/listtiles implemented

--- lib/screens/dashboard.screen.dart
+++ lib/screens/dashboard.screen.dart
@@ -8,16 +8,58 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SharedAppBar(
-        title: 'Dashboard Screen',
+        title: 'Dashboard',
         actions: [
           IconButton(icon: Icon(Icons.close), onPressed: () => Get.offAll(HomeScreen())),
         ],
       ),
       body: SafeArea(
-        child: Center(
-          child: Text('Dashboard Screen'),
-        ),
+        child: _buildBody(),
       ),
+    );
+  }
+
+  Widget _buildBody() {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 28),
+          child: Text(
+            'Good morning, AMIA',
+            style: Get.theme.textTheme.headline3,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        _listTile(
+          icon: Icons.ac_unit,
+          title: 'Check-in',
+          subtitle: 'Enter your temperature, symptoms, and relevant tests',
+        ),
+        _listTile(
+          icon: Icons.ac_unit,
+          title: 'History',
+          subtitle: 'See/edit your past entries',
+        ),
+        _listTile(
+          icon: Icons.ac_unit,
+          title: 'Contact tracing',
+          subtitle: 'Toggle method:\nlocation / bluetooth / both / off',
+        ),
+        _listTile(
+          icon: Icons.ac_unit,
+          title: 'Info',
+          subtitle: 'Learn more about COVID-19',
+        ),
+      ],
+    );
+  }
+
+  Widget _listTile({@required IconData icon, @required String title, @required String subtitle}) {
+    var _textTheme = Get.theme.textTheme;
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(title, style: _textTheme.subtitle1),
+      subtitle: Text(subtitle, style: _textTheme.subtitle2),
     );
   }
 }
