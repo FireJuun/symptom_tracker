a09b0933044d5ad7a33975b04357754b28ccd830
John Manning <john.d.manning@gmail.com>
2020-05-07 02:28:59 -0400

    flutter_icons added, listtiles complete

--- lib/screens/dashboard.screen.dart
+++ lib/screens/dashboard.screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:get/get.dart';
 import 'package:symptom_tracker/screens/screens.dart';
 import 'package:symptom_tracker/shared/shared.dart';
@@ -31,22 +32,24 @@ class DashboardScreen extends StatelessWidget {
           ),
         ),
         _listTile(
-          icon: Icons.ac_unit,
+          icon: Icons.check_circle_outline,
           title: 'Check-in',
           subtitle: 'Enter your temperature, symptoms, and relevant tests',
+          isGrey: true,
+          onPressed: () => Get.to(CheckinScreen()),
         ),
         _listTile(
-          icon: Icons.ac_unit,
+          icon: FlutterIcons.calendar_oct,
           title: 'History',
           subtitle: 'See/edit your past entries',
         ),
         _listTile(
-          icon: Icons.ac_unit,
+          icon: FlutterIcons.map_clock_outline_mco,
           title: 'Contact tracing',
           subtitle: 'Toggle method:\nlocation / bluetooth / both / off',
         ),
         _listTile(
-          icon: Icons.ac_unit,
+          icon: FlutterIcons.information_variant_mco,
           title: 'Info',
           subtitle: 'Learn more about COVID-19',
         ),
@@ -54,12 +57,28 @@ class DashboardScreen extends StatelessWidget {
     );
   }
 
-  Widget _listTile({@required IconData icon, @required String title, @required String subtitle}) {
-    var _textTheme = Get.theme.textTheme;
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(title, style: _textTheme.subtitle1),
-      subtitle: Text(subtitle, style: _textTheme.subtitle2),
+  Widget _listTile(
+      {@required IconData icon,
+      @required String title,
+      @required String subtitle,
+      bool isGrey,
+      void Function() onPressed}) {
+    var _txtTheme = Get.theme.textTheme;
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+      decoration: BoxDecoration(
+          border: Border.all(color: Get.theme.unselectedWidgetColor), borderRadius: BorderRadius.circular(16)),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          size: 24,
+          color: (isGrey ?? false) ? Get.theme.disabledColor : Get.theme.primaryColor,
+        ),
+        title: Text(title, style: _txtTheme.subtitle1),
+        subtitle: Text(subtitle, style: _txtTheme.subtitle2),
+        onTap: onPressed,
+      ),
     );
   }
 }
