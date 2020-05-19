f26ccb9d6708569cbecabaeb0881c0573a804653
John Manning <john.d.manning@gmail.com>
2020-05-19 17:05:30 -0400

    FHIR PUNS! Co-authored-by: grey faulkenberry <jgfaulkenberry@gmail.com> Co-authored-by: Jeff Nielson <jeffnielson@gmail.com> Co-authored-by: jinah703 <jinah811@gmail.com> Co-authored-by: adamatw <gh@adamwright.com> Co-authored-by: john.clark@vumc.org <john.clark@vumc.org> Co-authored-by: yukwongling <yukwongling@yahoo.com> Co-authored-by: mldorr <margaretdorr@gmail.com> Co-authored-by: lizzgilmore <lizzgilmore@gmail.com>

--- lib/screens/fhir/sandbox.dart
+++ lib/screens/fhir/sandbox.dart
@@ -19,12 +19,22 @@ class SandboxView extends StatelessWidget {
     return Expanded(
       child: ListView(
         children: <Widget>[
-          Text('text', textAlign: TextAlign.center),
-          Text('text', textAlign: TextAlign.center),
-          Text('text', textAlign: TextAlign.center),
-          Text('text', textAlign: TextAlign.center),
-          Text('text', textAlign: TextAlign.center),
-          Text('text', textAlign: TextAlign.center),
+          Text('FHIR Starter', textAlign: TextAlign.center),
+          Text('FHIR and Rain', textAlign: TextAlign.center),
+          Text('The FHIRplace', textAlign: TextAlign.center),
+          Text('Ring of FHIR', textAlign: TextAlign.center),
+          Text('FHIR Drills!', textAlign: TextAlign.center),
+          Text("We didn't start the FHIR", textAlign: TextAlign.center),
+          Text('Baptism by FHIR', textAlign: TextAlign.center),
+          Text('Rapid FHIR', textAlign: TextAlign.center),
+          Text('Please hold your FHIR until 1:30', textAlign: TextAlign.center),
+          Text('FHIR in the hole!', textAlign: TextAlign.center),
+          Text("Where there's smoke, there's FHIR", textAlign: TextAlign.center),
+          Text('The roof, the roof, the roof is on FHIR', textAlign: TextAlign.center),
+          Text('Fight FHIR with FHIR', textAlign: TextAlign.center),
+          Text('Please don\'t FHIR me, I really need this gig!', textAlign: TextAlign.center),
+          Text('Come on baby, light my FHIR', textAlign: TextAlign.center),
+          Text('Jeff is on FHIR!', textAlign: TextAlign.center),
         ],
       ),
     );
